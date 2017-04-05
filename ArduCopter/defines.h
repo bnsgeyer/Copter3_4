d16fa84588bb9eaf6016ddbc9b8946c7c73a7932
bnsgeyer <bnsgeyer@aol.com>
2017-04-04 22:26:16 -0400

    added enumeration for stabilize mode state

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -255,6 +255,15 @@ enum AcroModeState {
     Acro_Landed
 };
 
+// Stabilize states
+enum StabilizeModeState {
+    Stabilize_Unarmed,
+    Stabilize_MotorStopped,
+    Stabilize_Takeoff,
+    Stabilize_Flying,
+    Stabilize_Landed
+};
+
 // Flip states
 enum FlipState {
     Flip_Start,
